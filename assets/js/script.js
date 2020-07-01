ed9171355f59e104ae0d6b16bff9b626ad927d99
Michael Portu <michaelportu@Michaels-MacBook-Pro.local>
2020-07-01 14:15:00 -0500

    cards added delete task area added to bottom

--- assets/js/script.js
+++ assets/js/script.js
@@ -47,96 +47,97 @@ var saveTasks = function() {
 
 //Task update with click
 $(".list-group").on("click", "p", function(){
-  var text =$(this)
-    .text()
-    .trim();
-  var textInput =$("<textarea>")
-    .addClass("form-control")
-    .val(text);
+    var text =$(this)
+      .text()
+      .trim();
+    var textInput =$("<textarea>")
+      .addClass("form-control")
+      .val(text);
   
-  $(this).replaceWith(textInput);
-    textInput.trigger("focus");
+    $(this).replaceWith(textInput);
+     textInput.trigger("focus");
   });
 
+  //Task needs to be updated
 $(".list-group").on("blur", "textarea", function(){
   //get the textareas; current value/text
-  var text = $(this)
-    .val()
-    .trim();
+    var text = $(this)
+      .val()
+      .trim();
   
-  // get teh parent ul's id attribute
-  var status = $(this)
-    .closest(".list-group")
-    .attr("id")
-    .replace("list-", "");
+    // get teh parent ul's id attribute
+    var status = $(this)
+      .closest(".list-group")
+      .attr("id")
+      .replace("list-", "");
 
-  //get the task's position in the list of other li elements
-  var index = $(this)
-    .closest(".list-group-item")
-    .index();
+    //get the task's position in the list of other li elements
+    var index = $(this)
+      .closest(".list-group-item")
+      .index();
 
-  tasks[status][index].text = text;
-  saveTasks();
+    tasks[status][index].text = text;
+    saveTasks();
 
-  //recreate p element
-  var taskP = $("<p>")
-    .addClass("m-1")
-    .text(text);
+    //recreate p element
+    var taskP = $("<p>")
+      .addClass("m-1")
+      .text(text);
 
-  // replace textarea with p element
-  $(this).replaceWith(taskP);
+    // replace textarea with p element
+    $(this).replaceWith(taskP);
   });
 
 //due date update with click
 $(".list-group").on("click", "span", function(){
-  //get curent text
-  var date= $(this)
-    .text()
-    .trim();
+    //get curent text
+    var date= $(this)
+      .text()
+      .trim();
 
-  //create new imput element
-  var dateInput = $("<input>")
-    .attr("type", "text")
-    .addClass("form-control")
-    .val(date);
+    //create new imput element
+    var dateInput = $("<input>")
+      .attr("type", "text")
+      .addClass("form-control")
+      .val(date);
 
-  //swap out elements
-  $(this).replaceWith(dateInput);
+    //swap out elements
+    $(this).replaceWith(dateInput);
 
-  //automatically focus on new element
-  dateInput.trigger("focus");
+    //automatically focus on new element
+    dateInput.trigger("focus");
 });
 
 
 //value of due date was changed - update storage
 $(".list-group").on("blur", "input[type='text']", function(){
-  //get current text
-  var date = $(this)
-    .val()
-    .trim();
+    //get current text
+    var date = $(this)
+      .val()
+      .trim();
 
-  //get the parent ul's id attribute
-  var status = $(this)
-    .closest(".list-group")
-    .attr("id")
-    .replace("list-", "");
+    //get the parent ul's id attribute
+    var status = $(this)
+      .closest(".list-group")
+      .attr("id")
+      .replace("list-", "");
 
-  //get the task's positin in the list of other li elements
-  var index = $(this)
-    .closest(".list-group-item")
-    .index();
+    //get the task's positin in the list of other li elements
+    var index = $(this)
+      .closest(".list-group-item")
+      .index();
 
-  //update task in array and resave to localStorage
-  tasks[status][index].date = date;
-  saveTasks();
+    //update task in array and resave to localStorage
+    tasks[status][index].date = date;
+    saveTasks();
 
-  //recreate span element with bootstrap classes
-  var taskSpan =$("<span>")
-    .addClass("badge badge-primary badge-pill")
-    .text(date);
+    //recreate span element with bootstrap classes
+    var taskSpan =$("<span>")
+      .addClass("badge badge-primary badge-pill")
+      .text(date);
 
-  //replace input with span element
-  $(this).replaceWith(taskSpan);
+    //replace input with span element
+    $(this).replaceWith(taskSpan);
 });
 
 // modal was triggered
